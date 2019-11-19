5d97216ff1cb3ffd352387cff9ede7012620f734
Ardian Shala <ardian.sh@gmx.de>
2019-11-19 17:04:28 +0100

    cleanup

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,9 @@ import { initDB } from './sequelize';
 initDB()
 	.then(async () => {
 		try {
-			await Post.create({ title: 'Post 1', body: 'lorem ipsum lorem ipsum' })
-			const p = await Post.findByPk(1)
-			if (p) {
-				const c = await Comment.create({ title: 'Comment 1 for Post 1', body: 'lorem ipsum' })
-				await p.$add('comment', c);
-			}
+			const p = await Post.create({ title: 'Post 1', body: 'lorem ipsum lorem ipsum' })
+			const c = await Comment.create({ title: 'Comment 1 for Post 1', body: 'lorem ipsum' })
+			await p.$add('comment', c);
 		} catch (err) { console.error(err) }
-	});
+	})
+	.catch(err => console.error(err));
